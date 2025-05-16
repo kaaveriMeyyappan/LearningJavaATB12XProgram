67ca7c33c79789611525713e15de979a19cfd9f7
Kaaveri Meyyappan <kaaveri346@gmail.com>
2025-05-16 06:37:45 +0530

    updated program

---
+++ src/Task_001_Java_Multiples/tables.java
@@ -0,0 +1,20 @@
+package Task_001_Java_Multiples;
+
+public class tables {
+    public static void main (String[]args){
+        int num = 5;
+        System.out.printf("%-15s\n","multiples of 5:");
+        System.out.printf("%-5d",num*1);
+        System.out.printf("%-5d",num*2);
+        System.out.printf("%-5d",num*3);
+        System.out.printf("%-5d",num*4);
+        System.out.printf("%-5d",num*5);
+        System.out.printf("%-5d",num*6);
+        System.out.printf("%-5d",num*7);
+        System.out.printf("%-5d",num*8);
+        System.out.printf("%-5d",num*9);
+        System.out.printf("%-5d",num*10);
+
+
+    }
+}
--- src/ex_001_java_basics/helloworld.java
+++ src/ex_001_java_basics/helloworld.java
@@ -1,4 +1,7 @@
 package ex_001_java_basics;
 
 public class helloworld {
+    public static void main(String[]args){
+        System.out.println("Hello kaaveri");
+    }
 }
