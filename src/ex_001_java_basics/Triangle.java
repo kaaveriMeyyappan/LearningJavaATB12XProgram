bf5a0d7f8a0314443751c2e0d8f88a75682cc55e
Kaaveri Meyyappan <kaaveri346@gmail.com>
2025-05-23 19:38:43 +0530

    code updated

---
+++ src/ex_001_java_basics/Triangle.java
@@ -0,0 +1,26 @@
+package ex_001_java_basics;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first side of the triangle: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the second side of the triangle: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter the third side of the triangle: ");
+        int c = scanner.nextInt();
+        if (a == b && b == c) {
+            System.out.println("The triangle is Equilateral.");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("The triangle is Isosceles.");
+        } else {
+            System.out.println("The triangle is Scalene.");
+        }
+        scanner.close();
+    }
+}
+
+
+
