9d83a5e45a44398b39642a7434e5309f479448de
Kaaveri Meyyappan <kaaveri346@gmail.com>
2025-06-21 12:21:33 +0530

    code updated

---
+++ src/Codingchallenge/leapyear.java
@@ -0,0 +1,21 @@
+package Codingchallenge;
+
+public class leapyear {
+    public static void main(String[] args) {
+            int year = 2020; // Replace with the year you want to check
+
+            if (isLeapYear(year)) {
+                System.out.println(year + " is a leap year.");
+            } else {
+                System.out.println(year + " is not a leap year.");
+            }
+        }
+
+        public static boolean isLeapYear(int year) {
+            // A year is a leap year if:
+            // 1. It is divisible by 4, and
+            // 2. It is not divisible by 100, unless it is also divisible by 400
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        }
+    }
+
