b86acafd86313bea611e3bfe7e00c87ae032bce1
Kaaveri Meyyappan <kaaveri346@gmail.com>
2025-06-21 11:46:00 +0530

    code updated

---
+++ src/Codingchallenge/comparisonandlogical.java
@@ -0,0 +1,23 @@
+package Codingchallenge;
+
+public class comparisonandlogical {
+    public static void main(String[] args) {
+            // Comparison Operators
+            int a = 10, b = 20;
+            System.out.println("Comparison Operators:");
+            System.out.println("a == b: " + (a == b)); // Equal to
+            System.out.println("a != b: " + (a != b)); // Not equal to
+            System.out.println("a > b: " + (a > b));   // Greater than
+            System.out.println("a < b: " + (a < b));   // Less than
+            System.out.println("a >= b: " + (a >= b)); // Greater than or equal to
+            System.out.println("a <= b: " + (a <= b)); // Less than or equal to
+
+            // Logical Operators
+            boolean x = true, y = false;
+            System.out.println("\nLogical Operators:");
+            System.out.println("x && y: " + (x && y)); // Logical AND
+            System.out.println("x || y: " + (x || y)); // Logical OR
+            System.out.println("!x: " + (!x));         // Logical NOT
+        }
+    }
+
